ff52c3ae544bc0e6b64e1fce81d82866e3b02600
Rhys Thomas <rhys97@me.com>
2019-06-12 00:12:41 +0100

    Fixed home dir expansion bug.

--- cloner/cli.py
+++ cloner/cli.py
@@ -1,8 +1,11 @@
 import argparse
 from subprocess import call
+from os.path import expanduser
 
 
 def main():
+    home = expanduser("~")
+
     parser = argparse.ArgumentParser(
         description="Script to clone repos into my github folder, and also symbolically link to the desktop."
     )
@@ -10,16 +13,23 @@ def main():
     parser.add_argument(
         "--github",
         type=str,
-        default="~/Documents/GitHub",
+        default=f"{home}/Documents/GitHub",
         metavar="<path>",
         help="Path to your Github folder.",
     )
     parser.add_argument(
         "--desktop",
         type=str,
-        default="~/Desktop",
+        default=f"{home}/Desktop",
         metavar="<path>",
         help="Path to your Desktop folder.",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        default=False,
+        action="store_true",
+        help="Verbose output."
     )
 
     args = parser.parse_args()
@@ -33,7 +43,12 @@ def main():
     clone_cmd = f"git clone {repo} {gitdir}/{name}"
     symb_cmd = f"ln -s {gitdir}/{name} {deskdir}/{name}"
 
+    if args.verbose:
+        print(clone_cmd)
     call(clone_cmd.split(" "))
+
+    if args.verbose:
+        print(symb_cmd)
     call(symb_cmd.split(" "))
 
 
